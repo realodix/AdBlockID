8143720cd3b0328440fe5713e32b7bdc9f1a11ba
Realodix <realodix@yahoo.com>
2017-12-02 09:28:58 +0700

    rev-17.1202.0228

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
 import subprocess
 subprocess.call("flrender -i abid=. adblockid.template adblockid.txt")
 subprocess.call("flrender -i abid=. template/adblockid.template output/adblockid.txt")
+subprocess.call("flrender -i abid=. template/adblockid_lite.template output/adblockid_lite.txt")
