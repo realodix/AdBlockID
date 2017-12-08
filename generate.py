c3215c61ba10b4144e851773d423a5c3fe6cce52
Kei <realodix@yahoo.com>
2017-12-08 11:22:50 +0700

    rev-17.1208.0422

--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
 import subprocess
 subprocess.call("flrender -i abid=. adblockid.template adblockid.txt")
 subprocess.call("flrender -i abid=. template/adblockid.template output/adblockid.txt")
-subprocess.call("flrender -i abid=. template/adblockid_lite.template output/adblockid_lite.txt")
+subprocess.call("flrender -i abid=. template/adblockid_ultimate.template output/adblockid_ultimate.txt")
