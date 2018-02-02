b7a656903bb05c53853e19a8061a9b5d4fd43937
Kei <realodix@yahoo.com>
2018-02-03 00:43:43 +0700

    rev-18.0202.1743

--- generate.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-
-subprocess.call("flrender -i abid=. adblockid.template output/adblockid.txt")
--- vendor/python-abp/abp/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Container package for Python modules related to Adblock Plus.
-
-- abp.filters - Tools for working with Adblock Plus filter lists.
-"""
