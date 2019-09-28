713705570de071e1a899e0712981157f69a7337f
Kei <realodix@yahoo.com>
2019-09-28 15:01:43 +0700

    thirdparties-filter pindah ke folder src

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-flrender -i abid=. vendor/ffromvendor/filter.adbl ffromvendor.txt
+flrender -i abid=. src/template/thirdparties.adbl thirdparties-filter.txt
 
 # cleanup
-sed '/^!/d' -i ffromvendor.txt
-sed '/^#/d' -i ffromvendor.txt
-sed '/Adblock/d' -i ffromvendor.txt
+sed '/^!/d' -i thirdparties-filter.txt
+sed '/^#/d' -i thirdparties-filter.txt
+sed '/Adblock/d' -i thirdparties-filter.txt
 
 python vendor/fop/FOP.py
 
@@ -13,4 +13,4 @@ flrender -i abid=. src/template/adblockid.adbl output/adblockid.txt
 
 vendor/readme/readme.sh
 
-rm ffromvendor.txt
+rm thirdparties-filter.txt
