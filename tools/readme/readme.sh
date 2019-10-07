8ed079144afd8faa547594634b0591b7e01beefc
Kei <realodix@yahoo.com>
2019-10-07 18:03:55 +0700

    formatting

--- tools/readme/readme.sh
+++ tools/readme/readme.sh
@@ -3,16 +3,16 @@
 readonly readme_template="tools/readme/readme.template"
 readonly readme_temp="tools/readme/readme.tmp"
 
-timestamp=`date -u +'%b %d, %Y'`
+release_date=`date -u +'%b %d, %Y'`
 version=`date -u +'%y.%m.%d%H'`
 
-# add to readme
-sed -e "s/_timestamp_/$timestamp/g" -e "s/_version_/$version/g" $readme_template > $readme_temp
+# Buat file temporary
+sed -e "s/_release_date_/$release_date/g" -e "s/_version_/$version/g" $readme_template > $readme_temp
 
-# add to file
+# Ubah file temporary menjadi file readme yang sebenarnya
 cat $readme_temp > README.md
 echo "- Adding Date"
 echo "- Adding Version"
 
-# remove tmp file
+# Hapus file temporary
 rm -rf $readme_temp
