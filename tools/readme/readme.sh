a826cfbe28bdbcf268af4064eb4e93cb3f9a07af
Kei <realodix@yahoo.com>
2019-10-08 00:58:33 +0700

    versi minor jadi var tunggal

--- tools/readme/readme.sh
+++ tools/readme/readme.sh
@@ -3,11 +3,10 @@
 readonly readme_template="tools/readme/readme.template"
 readonly readme_temp="tools/readme/readme.tmp"
 
-a=`date -u +'%m'`
-b=`date -u +'%d'`
+v_minor=`date -u +'%d'`+`date -u +'%m'`-1
 
 release_date=`date -u +'%b %d, %Y'`
-version=`date -u +'%y.'`$((($a+$b)-1))`date -u +'.%H%M'`
+version=`date -u +'%y.'`$((v_minor))`date -u +'.%H%M'`
 
 # Buat file temporary
 sed -e "s/_release_date_/$release_date/g" -e "s/_version_/$version/g" $readme_template > $readme_temp
