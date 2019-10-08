7ad8c2e0ef78f3c4dfe1fdddb09c9f359e279e77
Kei <realodix@yahoo.com>
2019-10-08 18:17:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Render thirdparties filter
+# Render thirdparties filter dan buat file
+# thirdparties-filter.txt
 flrender -i abid=. src/template/thirdparties.adbl thirdparties-filter.txt
 
 # cleanup
@@ -9,8 +10,13 @@ sed '/^#/d' -i thirdparties-filter.txt
 sed '/Adblock/d' -i thirdparties-filter.txt
 python tools/fop/FOP.py
 
+# Gabungkan semua filter, termasuk thirdparties
+# filter
 flrender -i abid=. src/template/adblockid.adbl output/adblockid.txt
 
+# Update
 tools/readme/readme.sh
 
+# Hapus thirdparties-filter.txt, karena sudah
+# tidak digunakan lagi
 rm thirdparties-filter.txt
