754b5e1babac6db4f5a2bd7222c091a5f425f752
Kei <budi03@gmail.com>
2019-10-08 18:39:57 +0700

    Update the AdBlockID versioning method (#44)

--- tools/readme/readme.sh
+++ tools/readme/readme.sh
@@ -6,18 +6,22 @@
 
 
 readonly readme_template="tools/readme/readme.template"
-readonly readme_temp="tools/readme/readme.tmp"
+readonly readme_tmp="tools/readme/readme.tmp"
+
+v_mayor=`date -u +'%y'`
+v_minor=`date -u +'%j'`
+v_build=`date -u +'%H%M'`
+version=$((v_mayor)).$((v_minor)).$((v_build))
 
 release_date=`date -u +'%b %d, %Y'`
-version=`date -u +'%y.%m.%d%H'`
 
 # Buat file temporary
-sed -e "s/_release_date_/$release_date/g" -e "s/_version_/$version/g" $readme_template > $readme_temp
+sed -e "s/_release_date_/$release_date/g" -e "s/_version_/$version/g" $readme_template > $readme_tmp
 
 # Ubah file temporary menjadi file readme yang sebenarnya
-cat $readme_temp > README.md
+cat $readme_tmp > README.md
 echo "- Adding Date"
 echo "- Adding Version"
 
 # Hapus file temporary
-rm -rf $readme_temp
+rm -rf $readme_tmp
