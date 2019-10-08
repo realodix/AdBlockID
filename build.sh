408d6f6ae8a829c1e5c631ff8eae32dbe9cf274f
Kei <realodix@yahoo.com>
2019-10-08 18:11:13 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ flrender -i abid=. src/template/thirdparties.adbl thirdparties-filter.txt
 sed '/^!/d' -i thirdparties-filter.txt
 sed '/^#/d' -i thirdparties-filter.txt
 sed '/Adblock/d' -i thirdparties-filter.txt
-
 python tools/fop/FOP.py
 
 flrender -i abid=. src/template/adblockid.adbl output/adblockid.txt
