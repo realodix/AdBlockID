d79648fe09dab6608a87c9d80615735afe6ff367
Kei <realodix@yahoo.com>
2019-11-25 18:47:36 +0700

    Delete build - Copy.sh

--- build - Copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Khusus untuk AdGuard
-# https://github.com/AdguardTeam/FiltersRegistry/issues/259
-# https://github.com/AdguardTeam/FiltersCompiler/issues/41
-cp ./src/addons/adult-hide.adbl ./src/addons/adult-hide-adguard.adbl
-sed -i 's/" i]/"]/g' src/addons/adult-hide-adguard.adbl
