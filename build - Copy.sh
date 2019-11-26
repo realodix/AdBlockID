889c76c5eeb9d30bf512d69aa2b8e986d0ac90e2
Kei <realodix@yahoo.com>
2019-11-27 03:18:28 +0700

    Hapus IP

--- build - Copy.sh
+++ build - Copy.sh
@@ -5,6 +5,6 @@
 # https://github.com/AdguardTeam/FiltersCompiler/issues/41
 cp ./src/addons/adult-hide.adbl ./src/addons/adult-hide-adguard.adbl
 sed -i 's/" i]/"]/g' src/addons/adult-hide-adguard.adbl
-sed -i '/[1-9]\./d' src/addons/adult-hide-adguard.adbl
+sed -i '/"[0-9]\{1,3\}\./d' src/addons/adult-hide-adguard.adbl
 sed -i '/alt\*/d' src/addons/adult-hide-adguard.adbl
 sed -i '/title/d' src/addons/adult-hide-adguard.adbl
