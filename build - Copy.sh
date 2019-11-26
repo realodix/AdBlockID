bb8f27a5bb7fe0308d2b44364188cdde32ef2aae
Kei <realodix@yahoo.com>
2019-11-26 19:52:38 +0700

    Update build - Copy.sh

--- build - Copy.sh
+++ build - Copy.sh
@@ -5,4 +5,6 @@
 # https://github.com/AdguardTeam/FiltersCompiler/issues/41
 cp ./src/addons/adult-hide.adbl ./src/addons/adult-hide-adguard.adbl
 sed -i 's/" i]/"]/g' src/addons/adult-hide-adguard.adbl
-sed '/[1-9]{2,3}.*/d' -i src/addons/adult-hide-adguard.adbl
+sed -i '/[1-9]\./d' src/addons/adult-hide-adguard.adbl
+sed -i '/alt\*/d' src/addons/adult-hide-adguard.adbl
+sed -i '/title/d' src/addons/adult-hide-adguard.adbl
