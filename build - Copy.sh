c04ba6bbe6e8161036aec1013eb0f1dda0f4b6c4
Kei <realodix@yahoo.com>
2019-11-27 20:48:23 +0700

    Update build - Copy.sh

--- build - Copy.sh
+++ build - Copy.sh
@@ -3,10 +3,12 @@
 readonly aha="src/addons/adult-hide-adguard.adbl"
 
 cp ./src/addons/adult-hide.adbl ./src/addons/adult-hide-adguard.adbl
+
 sed -i 's/" i]/"]/g' $aha
 sed -i '/"[0-9]\{1,3\}\./d' $aha
 sed -i '/alt\*/d' $aha
 sed -i '/title/d' $aha
+
 sed -i '1s/^/! Jangan melakukan perubahan pada file ini (adult-hide-adguard.adbl), karena dibuat\n/' $aha
 sed -i '2s/^/! secara otomatis. Filter ini digunakan sebagai solusi untuk bug yang terdapat pada\n/' $aha
 sed -i '3s/^/! AdGuard FiltersCompiler yang menyebabkan beberapa filter pada AdBlockID  dianggap\n/' $aha
