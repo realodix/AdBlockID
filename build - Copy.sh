cb586d3167f0984ac00932294d64d95d7b1d4846
Kei <realodix@yahoo.com>
2019-11-27 20:33:02 +0700

    Update build - Copy.sh

--- build - Copy.sh
+++ build - Copy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Khusus untuk AdGuard
-# https://github.com/AdguardTeam/FiltersRegistry/issues/259
-# https://github.com/AdguardTeam/FiltersCompiler/issues/41
+readonly aha="src/addons/adult-hide-adguard.adbl"
+
 cp ./src/addons/adult-hide.adbl ./src/addons/adult-hide-adguard.adbl
-sed -i 's/" i]/"]/g' src/addons/adult-hide-adguard.adbl
-sed -i '/"[0-9]\{1,3\}\./d' src/addons/adult-hide-adguard.adbl
-sed -i '/alt\*/d' src/addons/adult-hide-adguard.adbl
-sed -i '/title/d' src/addons/adult-hide-adguard.adbl
+sed -i 's/" i]/"]/g' $aha
+sed -i '/"[0-9]\{1,3\}\./d' $aha
+sed -i '/alt\*/d' $aha
+sed -i '/title/d' $aha
+sed -i '1s/^/! Jangan melakukan perubahan pada file ini (adult-hide-adguard.adbl), karena dibuat\n/' $aha
+sed -i '2s/^/! secara otomatis. Filter ini digunakan sebagai solusi untuk bug yang terdapat pada\n/' $aha
+sed -i '3s/^/! AdGuard FiltersCompiler yang menyebabkan beberapa filter pada AdBlockID  dianggap\n/' $aha
+sed -i '4s/^/! tidak valid, sehingga tidak bisa disalurkan pada AdGuard.\n/' $aha
+sed -i '5s/^/!\n/' $aha
+sed -i '6s/^/! Info lebih lanjut:\n/' $aha
+sed -i '7s/^/! - https:\/\/github.com\/AdguardTeam\/FiltersRegistry\/issues\/259\n/' $aha
+sed -i '8s/^/! - https:\/\/github.com\/AdguardTeam\/FiltersCompiler\/issues\/41\n/' $aha
