824721c6b7286defc145d5aa9cd68673ab29a542
Kei <realodix@yahoo.com>
2019-11-29 19:31:11 +0700

    chore(adguard): format ulang dokumentasi

--- tools/adguard-compatibility/adguard-compatibility.sh
+++ tools/adguard-compatibility/adguard-compatibility.sh
@@ -14,10 +14,10 @@ sed -i '/alt\*/d' $aha
 sed -i '/title/d' $aha
 
 # Tulis waning pada file adult-hide-adguard.adbl
-sed -i '2s/^/! Jangan melakukan perubahan pada file ini (adult-hide-adguard.adbl), karena dibuat\n/' $aha
-sed -i '3s/^/! secara otomatis. Filter ini digunakan sebagai solusi untuk bug yang terdapat pada\n/' $aha
-sed -i '4s/^/! AdGuard FiltersCompiler yang menyebabkan beberapa filter pada AdBlockID  dianggap\n/' $aha
-sed -i '5s/^/! tidak valid, sehingga tidak bisa disalurkan pada AdGuard.\n/' $aha
+sed -i '2s/^/! Jangan melakukan perubahan di sini, karena karena file ini (adult-hide-adguard.adbl)\n/' $aha
+sed -i '3s/^/! dibuat secara otomatis. Filter ini digunakan sebagai solusi untuk bug yang terdapat\n/' $aha
+sed -i '4s/^/! pada AdGuard FiltersCompiler yang menyebabkan beberapa filter pada AdBlockID dianggap\n/' $aha
+sed -i '5s/^/! tidak valid, sehingga filter AdBlockID di AdGuard tidak bisa disalurkan pada AdGuard.\n/' $aha
 sed -i '6s/^/!\n/' $aha
 sed -i '7s/^/! Info lebih lanjut:\n/' $aha
 sed -i '8s/^/! - https:\/\/github.com\/AdguardTeam\/FiltersRegistry\/issues\/259\n/' $aha
