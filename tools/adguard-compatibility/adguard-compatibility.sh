6f610d5ef6529b7c615c140cee4a817be83b5db1
Kei <realodix@yahoo.com>
2019-11-29 19:22:40 +0700

    chore(adguard): Add beberapa dokumentasi

--- tools/adguard-compatibility/adguard-compatibility.sh
+++ tools/adguard-compatibility/adguard-compatibility.sh
@@ -2,13 +2,18 @@
 
 readonly aha="src/addons/adult-hide-adguard.adbl"
 
+# Copy semua filter pada adult-hide.adbl ke dalam adult-hide-adguard.adbl
 cp ./src/addons/adult-hide.adbl ./src/addons/adult-hide-adguard.adbl
 
+# Konversi filter
 sed -i 's/" i]/"]/g' $aha
+
+# Lakukan pembersihan filter yang tidak perlu
 sed -i '/"[0-9]\{1,3\}\./d' $aha
 sed -i '/alt\*/d' $aha
 sed -i '/title/d' $aha
 
+# Tulis waning pada file adult-hide-adguard.adbl
 sed -i '2s/^/! Jangan melakukan perubahan pada file ini (adult-hide-adguard.adbl), karena dibuat\n/' $aha
 sed -i '3s/^/! secara otomatis. Filter ini digunakan sebagai solusi untuk bug yang terdapat pada\n/' $aha
 sed -i '4s/^/! AdGuard FiltersCompiler yang menyebabkan beberapa filter pada AdBlockID  dianggap\n/' $aha
@@ -18,9 +23,12 @@ sed -i '7s/^/! Info lebih lanjut:\n/' $aha
 sed -i '8s/^/! - https:\/\/github.com\/AdguardTeam\/FiltersRegistry\/issues\/259\n/' $aha
 sed -i '9s/^/! - https:\/\/github.com\/AdguardTeam\/FiltersCompiler\/issues\/41\n/' $aha
 
+# Gunakan Pre-parsing directives untuk mencegah uBlock Origin dan turunannya
+# membaca rule pada adult-hide-adguard.adbl
 sed -i '1s/^/!#if false\n/' $aha
 sed -i '$s/$/\n!#endif/' $aha
 
-# Format ulang uBlock Pre-parsing directives yang ngaco setelah filter dirender
+# Format ulang penulisan uBlock Pre-parsing directives yang jadi terlihat typo
+# setelah filter dirender oleh python-abp
 sed -i 's/! #if false/!#if false/g' output/adblockid.txt
 sed -i 's/! #endif/!#endif/g' output/adblockid.txt
