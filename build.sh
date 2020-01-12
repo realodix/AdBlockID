6c576f9b663458d817cbc693e6c7ee59f01553c9
Kei <budi03@gmail.com>
2020-01-12 18:59:36 +0700

    chore(tools): Delete Hoshsadiq - NoCoin adblock list (3p filter) (#256)

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Render thirdparties filter dan buat file thirdparties-filter.txt
-flrender -i abid=. src/template/thirdparties.adbl thirdparties-filter.txt
-
 # cleanup
-sed '/^!/d' -i thirdparties-filter.txt
-sed '/^#/d' -i thirdparties-filter.txt
-sed '/Adblock/d' -i thirdparties-filter.txt
 python tools/fop/FOP.py
 
 # Gabungkan semua filter, termasuk thirdparties filter
@@ -14,6 +8,3 @@ flrender -i abid=. src/template/adblockid.adbl output/adblockid.txt
 
 # Update versi dan tanggal pada file readme
 tools/readme/readme.sh
-
-# Hapus thirdparties-filter.txt, karena sudah tidak digunakan lagi
-rm thirdparties-filter.txt
