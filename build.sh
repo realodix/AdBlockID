c4800ac24c31c1b78aa8998124549d72d3e6f68a
Kei <realodix@yahoo.com>
2020-01-28 18:06:03 +0700

    dalete readme generator

--- build.sh
+++ build.sh
@@ -5,6 +5,3 @@ python tools/fop/FOP.py
 
 # Gabungkan semua filter, termasuk thirdparties filter
 flrender -i abid=. src/template/adblockid.adbl output/adblockid.txt
-
-# Update versi dan tanggal pada file readme
-tools/readme/readme.sh
