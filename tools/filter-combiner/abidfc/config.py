ee52a79f18d8a3275ef71bacb7bacd61b1fcbb9a
Kei <budi03@gmail.com>
2021-11-09 20:26:31 +0700

    Update config.py

--- tools/filter-combiner/abidfc/config.py
+++ tools/filter-combiner/abidfc/config.py
@@ -1,7 +1,5 @@
-# Store the version here so:
-# 1) we don't load dependencies by storing it in __init__.py
-# 2) we can import it in setup.py for the same reason
-# 3) we can import it into module
+""" Konfigurasi
+"""
 
 config = {
     'libName'      : 'AdBlockID Filter Combiner',
@@ -9,4 +7,9 @@ config = {
     'revisedTarget': './output/adblockid.txt'
 }
 
+
+# Store the version here so:
+# 1) we don't load dependencies by storing it in __init__.py
+# 2) we can import it in setup.py for the same reason
+# 3) we can import it into module
 __version__   = config['libVersion']
