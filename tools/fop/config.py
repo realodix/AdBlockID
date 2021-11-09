885fbb02ba6d26844cc0d6eb5bb2cdc1bfacab36
Kei <budi03@gmail.com>
2021-11-10 04:23:26 +0700

    tools(fop): Pisahkan global varible ke config.py

---
+++ tools/fop/config.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+""" Filter Orderer and Preener
+
+Adjusted for AdBlockID
+Based on FOP (RU AdList) v3.921
+
+Copyright (C) 2011 Michael
+GNU General Public License
+"""
+
+import re
+
+VERSION = "1.7"
+SECTIONS_EXT = [".txt", ".adbl"]
+# List the files that should not be sorted, either because they have a special sorting
+# system or because they are not filter files
+IGNORE = ("adblockid.txt", "docs", "tools", "template", "international.adbl", "python-abp"
+    "python-abp", "python-abp_AdBlockID", "VICHS_AdBlockID")
+
+# Regex to match important filter parts
+ELEMENTDOMAINPATTERN = re.compile(r"^([^\/\*\|\@\"\!]*?)#[@$?]?#")
+FILTERDOMAINPATTERN = re.compile(r"(?:\$|\,)domain\=([^\,\s]+)$")
+ELEMENTPATTERN = re.compile(r"^([^\/\*\|\@\"\!]*?)(#[@$?]?#)([^{}]+)$")
+OPTIONPATTERN = re.compile(r"^(.*)\$(~?[\w\-]+(?:=[^,\s]+)?(?:,~?[\w\-]+(?:=[^,\s]+)?)*)$")
+REDIWRITEOPTIONPATTERN = re.compile(r"^(redirect(-rule)?|rewrite)=")
+
+# Regex to match element tags, pseudo classes, strings and tree selectors; "@" indicates
+# either the beginning or the end of a selector
+SELECTORPATTERN = re.compile(r"""
+  (?<=[\s\[@])
+  ([a-zA-Z]*[A-Z][a-zA-Z0-9]*)
+  (
+    (?=([\[\]\^\*\$=:@#\.]))
+    | (?=(\s(
+      ?:[+>~]
+      | \*|[a-zA-Z][a-zA-Z0-9]*[\[:@\s#\.]
+      | [#\.][a-zA-Z][a-zA-Z0-9]*
+    )))
+  )
+""")
+PSEUDOPATTERN = re.compile(r"(\:[a-zA-Z\-]*[A-Z][a-zA-Z\-]*)(?=([\(\:\@\s]))")
+# (?!:-) - skip ABP `:-abp-...` pseudoclasses
+# (?!:style\() - skip uBO `:style()` pseudoclass
+REMOVE_AST_PATTERN = re.compile(r"""
+  (
+    (?<=([>+~,]\s))
+    | (?<=(@|\s|,))
+  )
+  (\*)
+  (?=([#\.\[\:]))
+  (?!:-)
+  (?!:style\()
+""")
+SELECTORSTYLEPART = re.compile(r":style\(.+\)$")
+REMOVE_0PX_PATTERN = re.compile(r"((?<=([\:\s]0))(px)(?=([\s\!])))")
+BANGSPACEIMPORTANT = re.compile(r"(.)(\!\s)(important)")
+ATTRIBUTEVALUEPATTERN = re.compile(r"^([^\'\"\\]|\\.)*(\"(?:[^\"\\]|\\.)*\"|\'(?:[^\'\\]|\\.)*\')")
+TREESELECTOR = re.compile(r"(\\.|[^\+\>\~\\\ \t])\s*([\+\>\~\ \t])\s*(\D)")
+UNICODESELECTOR = re.compile(r"\\[0-9a-fA-F]{1,6}\s[a-zA-Z]*[A-Z]")
+NONSELECTOR = re.compile(r"^(\+js\(|script:inject\()")
+SELECTORANDTAILPATTERN = re.compile(r"^(.*?)((:-abp-contains|:style|:matches-css)(.*))?$")
+
+# Regex that describes a completely blank line
+BLANKPATTERN = re.compile(r"^\s*$")
+
+# List all options (excepting domain and `KNOWNPARAMETERS`, which is handled separately)
+KNOWNOPTIONS = (
+    # ABP
+    # https://help.eyeo.com/en/adblockplus/how-to-write-filters#options
+    "document", "elemhide", "font", "genericblock", "generichide", "image", "match-case",
+    "media", "object", "other", "ping", "popup", "script", "stylesheet", "subdocument",
+    "third-party", "webrtc", "websocket", "xmlhttprequest",
+
+    # uBlock Origin
+    # https://github.com/gorhill/uBlock/wiki/Static-filter-syntax
+    "1p", "first-party", "strict1p", "3p", "strict3p", "all", "badfilter", "cname", "csp",
+    "css", "doc", "ehide", "frame", "ghide", "important", "inline-font", "inline-script",
+    "mp4", "object-subrequest", "popunder", "shide", "specifichide", "xhr"
+)
+
+# List of known key=value parameters (domain is not included)
+KNOWNPARAMETERS = (
+    # ABP
+    "rewrite",
+
+    # uBO
+    "csp", "denyallow", "redirect", "redirect-rule", "removeparam"
+)
--- tools/fop/fop.py
+++ tools/fop/fop.py
@@ -1,70 +1,15 @@
 #!/usr/bin/env python3
-""" FOP AdBlockID
+""" Filter Orderer and Preener
 
 Adjusted for AdBlockID
-Based on FOP RU AdList v3.921
+Based on FOP (RU AdList) v3.921
 
 Copyright (C) 2011 Michael
 GNU General Public License
 """
 
 import collections, filecmp, os, re, subprocess, sys
-
-VERSION = "1.7"
-SECTIONS_EXT = [".txt", ".adbl"]
-
-# Compile regular expressions to match important filter parts (derived from Wladimir
-# Palant's Adblock Plus source code)
-ELEMENTDOMAINPATTERN = re.compile(r"^([^\/\*\|\@\"\!]*?)#[@$?]?#")
-FILTERDOMAINPATTERN = re.compile(r"(?:\$|\,)domain\=([^\,\s]+)$")
-ELEMENTPATTERN = re.compile(r"^([^\/\*\|\@\"\!]*?)(#[@$?]?#)([^{}]+)$")
-OPTIONPATTERN = re.compile(r"^(.*)\$(~?[\w\-]+(?:=[^,\s]+)?(?:,~?[\w\-]+(?:=[^,\s]+)?)*)$")
-REDIWRITEOPTIONPATTERN = re.compile(r"^(redirect(-rule)?|rewrite)=")
-
-# Compile regular expressions that match element tags and pseudo classes and strings and
-# tree selectors; "@" indicates either the beginning or the end of a selector
-SELECTORPATTERN = re.compile(r"(?<=[\s\[@])([a-zA-Z]*[A-Z][a-zA-Z0-9]*)((?=([\[\]\^\*\$=:@#\.]))|(?=(\s(?:[+>~]|\*|[a-zA-Z][a-zA-Z0-9]*[\[:@\s#\.]|[#\.][a-zA-Z][a-zA-Z0-9]*))))")
-PSEUDOPATTERN = re.compile(r"(\:[a-zA-Z\-]*[A-Z][a-zA-Z\-]*)(?=([\(\:\@\s]))")
-# (?!:-) - skip Adblock Plus' :-abp-... pseudoclasses
-# (?!:style\() - skip uBo's :style() pseudoclass
-REMOVE_AST_PATTERN = re.compile(r"((?<=([>+~,]\s))|(?<=(@|\s|,)))(\*)(?=([#\.\[\:]))(?!:-)(?!:style\()")
-SELECTORSTYLEPART = re.compile(r":style\(.+\)$")
-REMOVE_0PX_PATTERN = re.compile(r"((?<=([\:\s]0))(px)(?=([\s\!])))")
-BANGSPACEIMPORTANT = re.compile(r"(.)(\!\s)(important)")
-ATTRIBUTEVALUEPATTERN = re.compile(r"^([^\'\"\\]|\\.)*(\"(?:[^\"\\]|\\.)*\"|\'(?:[^\'\\]|\\.)*\')")
-TREESELECTOR = re.compile(r"(\\.|[^\+\>\~\\\ \t])\s*([\+\>\~\ \t])\s*(\D)")
-UNICODESELECTOR = re.compile(r"\\[0-9a-fA-F]{1,6}\s[a-zA-Z]*[A-Z]")
-NONSELECTOR = re.compile(r"^(\+js\(|script:inject\()")
-SELECTORANDTAILPATTERN = re.compile(r"^(.*?)((:-abp-contains|:style|:matches-css)(.*))?$")
-
-# Compile a regular expression that describes a completely blank line
-BLANKPATTERN = re.compile(r"^\s*$")
-
-# List the files that should not be sorted, either because they have a special sorting
-# system or because they are not filter files
-IGNORE = ("adblockid.txt", "docs", "tools", "template", "international.adbl",
-          "python-abp", "python-abp_AdBlockID", "VICHS_AdBlockID")
-
-# List all options (excepting domain, which is handled separately)
-KNOWNOPTIONS = (
-    # ABP
-    # https://help.eyeo.com/en/adblockplus/how-to-write-filters#options
-    "document", "elemhide", "font", "genericblock", "generichide", "image", "match-case", "media", "object", "other", "ping", "popup", "script", "stylesheet", "subdocument", "third-party", "webrtc", "websocket", "xmlhttprequest",
-
-    # uBlock Origin
-    # https://github.com/gorhill/uBlock/wiki/Static-filter-syntax
-    "1p", "first-party", "strict1p", "3p", "strict3p", "all", "badfilter", "cname", "csp", "css", "doc", "ehide", "frame", "ghide", "important", "inline-font", "inline-script", "mp4", "object-subrequest", "popunder", "shide", "specifichide", "xhr"
-)
-
-# List of known key=value parameters (domain is not included)
-KNOWNPARAMETERS = (
-    # ABP
-    "rewrite",
-
-    # uBO
-    "csp", "denyallow", "redirect", "redirect-rule", "removeparam"
-)
-
+from config import *
 
 def start ():
     """ Print a greeting message and run FOP in the directories specified via the command
