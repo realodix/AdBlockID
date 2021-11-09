a6dd7ace652be16df4629ded483730c58a9445a9
Kei <budi03@gmail.com>
2021-11-09 20:25:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 python tools/fop/fop.py
 
 # Gabungkan semua filter, termasuk thirdparties filter
-flrender -i abid=. template/adblockid.adbl output/adblockid.txt
+flcombine -i abid=. template/adblockid.adbl output/adblockid.txt
