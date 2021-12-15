dd91ec8e8444052842482e5ed32e3f092ee19934
Kei <budi03@gmail.com>
2021-12-15 20:38:46 +0700

    Rename template file to adblockid.template.txt

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 python tools/fop/fop.py
 
 # Gabungkan semua filter, termasuk thirdparties filter
-flcombine -i abid=. template/adblockid.adbl output/adblockid.txt
+flcombine -i abid=. template/adblockid.template.txt output/adblockid.txt
