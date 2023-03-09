270a5ad4a6f26e79b4f05d011a2b1225ae4f0e99
Kei <budi03@gmail.com>
2023-03-09 18:40:34 +0700

    Update shell command following latest FOP v1.8 update (#1351)

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# cleanup
-python tools/fop/fop.py
+FOP="python tools/fop/fop.py -d src output -i adblockid.txt adblockid_plus.txt personal.txt"
 
-# Gabungkan semua filter, termasuk thirdparties filter
-flcombine -i abid=. template/adblockid.template.txt output/adblockid.txt
-flcombine -i abid=. src/packages/adblockid_plus.template.txt output/adblockid_plus.txt
+if [ "$1" == "fop" ]; then
+    eval $FOP
+fi
+
+if [ "$1" == "abid" ]; then
+    eval $FOP
+    eval "flcombine -i abid=. template/adblockid.template.txt output/adblockid.txt"
+fi
+
+if [ "$1" == "abidplus" ]; then
+    eval $FOP
+    eval "flcombine -i abid=. src/packages/adblockid_plus.template.txt output/adblockid_plus.txt"
+fi
+
+read -t 7 -p "Press any key to continue... " -n 1
