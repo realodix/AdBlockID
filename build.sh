90ae144563597aed0251cd29cb0850b3cfbba83d
Kei <budi03@gmail.com>
2023-03-09 18:24:11 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-FOP= python tools/fop/fop.py -d src output -i adblockid.txt adblockid_plus.txt personal.txt
-# ABID= 'flcombine -i abid=. template/adblockid.template.txt output/adblockid.txt'
-# ABID_PLUS= flcombine -i abid=. src/packages/adblockid_plus.template.txt output/adblockid_plus.txt
-
+FOP="python tools/fop/fop.py -d src output -i adblockid.txt adblockid_plus.txt personal.txt"
 
 if [ "$1" == "fop" ]; then
-    eval "$FOP"
+    eval $FOP
 fi
 
 if [ "$1" == "abid" ]; then
-    eval "$FOP"
+    eval $FOP
     eval "flcombine -i abid=. template/adblockid.template.txt output/adblockid.txt"
 fi
 
 if [ "$1" == "abidplus" ]; then
-    eval "$FOP"
+    eval $FOP
     eval "flcombine -i abid=. src/packages/adblockid_plus.template.txt output/adblockid_plus.txt"
 fi
 
