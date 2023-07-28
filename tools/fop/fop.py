d449009b2ae8678e47462790700ed2a70cb2196e
Kei <budi03@gmail.com>
2023-07-28 13:40:37 +0700

    Improve `FILTERDOMAINPATTERN`

--- tools/fop/fop.py
+++ tools/fop/fop.py
@@ -36,7 +36,7 @@ FILE_EXTENSION = [".adfl", ".txt"]
 # Compile regular expressions to match important filter parts
 # (derived from Wladimir Palant's Adblock Plus source code)
 ELEMENTDOMAINPATTERN = re.compile(r"^([^\/\*\|\@\"\!]*?)(#|\$)\@?\??\@?(#|\$)")
-FILTERDOMAINPATTERN = re.compile(r"(?:\$|\,)(?:domain|from)\=([^\,\s]+)$")
+FILTERDOMAINPATTERN = re.compile(r"(?:\$|\,)(denyallow|domain|from|method|to)\=([^\,\s]+)$")
 ELEMENTPATTERN = re.compile(
     r"^([^\/\*\|\@\"\!]*?)(\$\@?\$|##\@?\$|#[\@\?]?#\+?)(.*)$")
 OPTIONPATTERN = re.compile(
