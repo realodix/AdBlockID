2d6cc3289f3f3d6787efd2e9a3bd482c0969749a
Kei <budi03@gmail.com>
2023-07-29 01:31:04 +0700

    `$from` ada di depan `$denyallow`

--- tools/fop/fop.py
+++ tools/fop/fop.py
@@ -342,10 +342,10 @@ def filtertidy(filterin, filename):
     # If applicable, sort domain restrictions and append them to the list of options
     if domainlist:
         optionlist.append(f'domain={"|".join(sorted(set(filter(lambda domain: domain != "", domainlist)), key=lambda domain: domain.strip("~")))}')
-    if denyallowlist:
-        optionlist.append(f'denyallow={"|".join(sorted(set(filter(lambda domain: domain != "", denyallowlist)), key=lambda domain: domain.strip("~")))}')
     if fromlist:
         optionlist.append(f'from={"|".join(sorted(set(filter(lambda domain: domain != "", fromlist)), key=lambda domain: domain.strip("~")))}')
+    if denyallowlist:
+        optionlist.append(f'denyallow={"|".join(sorted(set(filter(lambda domain: domain != "", denyallowlist)), key=lambda domain: domain.strip("~")))}')
     if tolist:
         optionlist.append(f'to={"|".join(sorted(set(filter(lambda domain: domain != "", tolist)), key=lambda domain: domain.strip("~")))}')
     if methodlist:
